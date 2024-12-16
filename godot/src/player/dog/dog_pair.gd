7e4cdc74ea81c8765d98931f30a37061f0a1a36b
Ángela <angela.lbsr@gmail.com>
2024-12-16 11:13:15 +0100

    refactor sled movement

--- godot/src/player/dog/dog_pair.gd
+++ godot/src/player/dog/dog_pair.gd
@@ -1,10 +1,21 @@
-extends CharacterBody2D
+class_name DogPair extends Node2D
 
 @onready var lead: Line2D = $Lead
 @onready var left_dog: AnimatedSprite2D = $LeftDog
 @onready var right_dog: AnimatedSprite2D = $RightDog
+@onready var lead_marker: Marker2D = $LeadPosition
+
+
+func _ready() -> void:
+	lead.points[1] = lead_marker.position
+	
 
 func _physics_process(_delta: float) -> void:
 	lead.points[0] = left_dog.position - Vector2(2,2)
 	lead.points[2] = right_dog.position - Vector2(2,2)
+	
+
+func follow_at_distance(lead: DogPair, distance: float) -> void:
+	if global_position.distance_squared_to(lead.global_position) > pow(distance, 2):
+		global_position = lead.global_position + distance * lead.global_position.direction_to(global_position)
 	
--- godot/src/player/player.gd
+++ godot/src/player/player.gd
@@ -10,21 +10,22 @@ extends Node2D
 @export var turn_speed:= 40
 @export var speed:= 200
 
-var dog_pairs: Array[CharacterBody2D]
+var dog_pairs: Array[DogPair]
 var is_alive:= false
 
 @onready var lead: Line2D = $Lead
 @onready var sled: Area2D = $Sled
 
+
 func _ready() -> void:
 	sled.area_entered.connect(_on_sled_area_entered)
 	
-	for i in dog_pair_number:
-		_add_dog_pair(Vector2(-distance * i, 0))
+	lead.add_point(Vector2.ZERO)
+	_add_dog_pair(Vector2(sled_distance, 0))
 	
-	sled.position = Vector2(- sled_distance - distance * dog_pair_number, 4)
-	lead.add_point(sled.position)
-	
+	for i in range(1, dog_pair_number):
+		_add_dog_pair(Vector2(sled_distance + i * distance, 0))
+		
 	is_alive = true
 	
 
@@ -32,37 +33,33 @@ func _physics_process(delta: float) -> void:
 	if not is_alive:
 		return
 	
-	var target_positions: Array[Vector2]
+	sled.position = _follow(sled.position, dog_pairs.front().position, sled_distance)
 	
+	for i in dog_pair_number - 1:
+		dog_pairs[i].follow_at_distance(dog_pairs[i+1], distance)
+		
 	var input = Input.get_axis("move_left", "move_right")
 	var velocity = Vector2(speed, input * turn_speed)
-	target_positions.append(dog_pairs.front().global_position + delta * velocity)
+	dog_pairs.back().position += delta * velocity
 	
-	for i in dog_pair_number - 1:
-		var current_position = dog_pairs[i + 1].global_position
-		target_positions.append(_target_position(current_position, target_positions.back(), distance))
-	
-	target_positions.append(_target_position(sled.global_position, target_positions.back(), sled_distance) + Vector2(0, 1))
-	
-	lead.clear_points()
-	
-	global_position = target_positions.front()
-	lead.add_point(Vector2.ZERO)
-	
-	for i in dog_pair_number - 1:
-		dog_pairs[i+1].global_position = target_positions[i+1]
-		lead.add_point(target_positions[i+1] - global_position)
-		
-	
-	sled.global_position = target_positions.back()
-	lead.add_point(target_positions.back() - global_position)
+	_recalibrate_positions()
 	
 
-func _target_position(current_position: Vector2, lead_position: Vector2, dist: float) -> Vector2:
+func _follow(current_position: Vector2, lead_position: Vector2, dist: float) -> Vector2:
 	if current_position.distance_squared_to(lead_position) > pow(dist, 2):
 		return lead_position + dist * lead_position.direction_to(current_position)
 	else:
 		return current_position
+	
+
+func _recalibrate_positions() -> void:
+	var sled_offset = sled.position
+	global_position = sled.global_position
+	sled.position = Vector2.ZERO
+	
+	for i in dog_pair_number:
+		dog_pairs[i].position -= sled_offset
+		lead.points[i+1] = dog_pairs[i].lead_marker.global_position - global_position
 	
 
 func _add_dog_pair(at_position: Vector2) -> void:
