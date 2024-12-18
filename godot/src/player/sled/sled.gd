0ffa8c4320e679ec9186aa31983e74b10d8ba197
Claudio Coelho <erebrus@gmail.com>
2024-12-18 13:39:58 +0000

    sled snow sprites

--- godot/src/player/sled/sled.gd
+++ godot/src/player/sled/sled.gd
@@ -4,9 +4,13 @@ class_name Sled extends Area2D
 @export var follow_component: FollowComponent
 @export var jump_component: JumpComponent
 
+@onready var snow_sprite: AnimatedSprite2D = $SnowSprite
+
 var height: float:
 	set(value):
 		if value != height:
 			height = value
 			$JumpingBits.position.y = height
 	
+func _ready():
+	snow_sprite.play("run")
