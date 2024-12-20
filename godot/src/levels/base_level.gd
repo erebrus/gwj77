e7f9121d0b68a33bc39a6216456e3d451d868d82
Cláudio Coelho <claudio.coelho@solenix.ch>
2024-12-20 21:17:20 +0000

    present generation

--- godot/src/levels/base_level.gd
+++ godot/src/levels/base_level.gd
@@ -3,8 +3,9 @@ extends Node2D
 @export var TreeScene: PackedScene
 @export var LogScene: PackedScene
 @export var SnowPileScene: PackedScene
-@export var MooseScene:PackedScene
-@export var RabbitScene:PackedScene
+@export var PresentScene: PackedScene
+#@export var MooseScene:PackedScene
+#@export var RabbitScene:PackedScene
 
 func _ready() -> void:
 	
@@ -15,6 +16,8 @@ func _ready() -> void:
 			_place_obstacle_around(TreeScene, Vector2(i, 160))
 		else:
 			_place_obstacle_around(LogScene, Vector2(i, 160))
+		if randf() < 0.1:
+			_place_obstacle_around(PresentScene, Vector2(i, 160))
 	
 
 func _place_obstacle_around(obstacle_scene: PackedScene, at_position: Vector2) -> void:
