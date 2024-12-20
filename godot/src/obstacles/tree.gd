ab88be63301907ebad68123978e71fa7d7386982
Ángela <angela.lbsr@gmail.com>
2024-12-20 22:31:22 +0100

    integrate trees and avalanche

--- godot/src/obstacles/tree.gd
+++ godot/src/obstacles/tree.gd
@@ -1,6 +1,19 @@
 extends Node2D
 
+@export var textures: Array[Texture]
 
+
+@onready var sprite := $Sprite2D
+
+func _ready():
+	$Sprite2D.texture = textures.pick_random()
+	sprite.material = sprite.material.duplicate()
+	
 func enter(sled: Sled) -> void:
 	sled.get_parent().crash()
 	
+
+func _on_shake_area_area_exited(area):
+	sprite.material.set_shader_parameter("strength", 3)
+	await get_tree().create_timer(1).timeout
+	sprite.material.set_shader_parameter("strength", 0)
