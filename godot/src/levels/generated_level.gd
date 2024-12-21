c24764a61b2545e5385594be6d163e06e3fb01ff
Ángela <angela.lbsr@gmail.com>
2024-12-21 14:21:04 +0100

    dont put logs on top of snowpiles

--- godot/src/levels/generated_level.gd
+++ godot/src/levels/generated_level.gd
@@ -7,7 +7,8 @@ extends BaseLevel
 @export var PresentScene: PackedScene
 
 @export_category("Generation")
-@export var grid_size:= 40
+@export var grid_size:= 32
+@export var cells_between_log_and_snow:= 4
 @export var screens_in_advance:= 1.5
 @export var templates: Array[Texture2D]
 
@@ -19,6 +20,9 @@ var grid: Dictionary # Dictionary[Vector2, Node]
 @onready var cells_per_screen = 640.0 / grid_size
 @onready var cells_in_advance = screens_in_advance * cells_per_screen
 @onready var checkpoint = -cells_in_advance
+
+
+var previous_obstacles: Dictionary
 
 
 func _ready() -> void:
@@ -38,6 +42,7 @@ func _place_section() -> void:
 	var template: Texture2D = templates.pick_random()
 	var image: Image = template.get_image()
 	var template_size = template.get_size()
+	var obstacles: Dictionary
 	
 	Logger.info("Placing section %s at %s" % [template.get_path().get_file(), start_cell])
 	
@@ -52,14 +57,18 @@ func _place_section() -> void:
 			else:
 				if randf() < pixel.b:
 					if randf() < 0.5:
-						_place_obstacle_on_cell(SnowPileScenes.pick_random(), cell)
+						if not _has_obstacle_in_range(Types.Obstacle.Log, cell, obstacles):
+							_place_obstacle_on_cell(SnowPileScenes.pick_random(), cell)
+							obstacles[cell] = Types.Obstacle.Snowpile
 					else:
-						_place_obstacle_on_cell(LogScene, cell)
+						if not _has_obstacle_in_range(Types.Obstacle.Snowpile, cell, obstacles):
+							_place_obstacle_on_cell(LogScene, cell)
+							obstacles[cell] = Types.Obstacle.Log
 				if randf() < pixel.r:
 					_place_obstacle_on_cell(PresentScene, cell)
 	
+	previous_obstacles = obstacles
 	checkpoint = start_cell.x + template_size.x - cells_in_advance
-	
 	
 
 func _place_obstacle_on_cell(obstacle_scene: PackedScene, cell: Vector2) -> void:
@@ -70,3 +79,18 @@ func _place_obstacle_on_cell(obstacle_scene: PackedScene, cell: Vector2) -> void
 	obstacle.position.y = at_position.y + randi_range(- grid_size / 2, grid_size / 2)
 	add_child(obstacle)
 	
+
+func _has_obstacle_in_range(type: Types.Obstacle, cell: Vector2, obstacles: Dictionary) -> bool:
+	for x in range(cell.x - cells_between_log_and_snow, cell.x):
+		for y in range(cell.y - cells_between_log_and_snow, cell.y + cells_between_log_and_snow):
+			var c = Vector2(x,y)
+			if previous_obstacles.has(c) and previous_obstacles[c] == type:
+				return true
+	
+	for x in range(cell.x - cells_between_log_and_snow, cell.x):
+		for y in range(cell.y - cells_between_log_and_snow, cell.y + cells_between_log_and_snow):
+			var c = Vector2(x,y)
+			if obstacles.has(c) and obstacles[c] == type:
+				return true
+	
+	return false
