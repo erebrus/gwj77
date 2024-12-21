09ba1ea61de08be338b3324ca7ed61aa0a605c6f
Ángela <angela.lbsr@gmail.com>
2024-12-21 18:52:43 +0100

    stamina meter

--- godot/src/common/events.gd
+++ godot/src/common/events.gd
@@ -12,6 +12,8 @@ signal present_captured
 
 @warning_ignore("unused_signal")
 signal stamina_changed(value:float)
+@warning_ignore("unused_signal")
+signal stamina_exhaustion_finished
 
 @warning_ignore("unused_signal")
 signal ui_configuration_updated
