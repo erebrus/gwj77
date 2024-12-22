25bb84e27f55bff097fb4e5446c5f8a7890e66e6
Cláudio Coelho <claudio.coelho@solenix.ch>
2024-12-22 10:23:05 +0000

    avalanche distances

--- godot/src/levels/avalanche/avalanche.gd
+++ godot/src/levels/avalanche/avalanche.gd
@@ -2,12 +2,15 @@ extends Node2D
 
 
 @export var speed: float = 100
-
+@export var far_distance := 900.0
+@export var critical_distance := 300.0
 
 @onready var hitbox = $HitBox
 @onready var sprite = $Parallax2D
 @onready var closing_in: Area2D = $ClosingIn
 @onready var close: Area2D = $Close
+
+var current:=Types.GameMusic.NORMAL
 
 func _ready() -> void:
 	hitbox.area_entered.connect(_on_hitbox_entered)
@@ -15,30 +18,31 @@ func _ready() -> void:
 
 func _physics_process(delta: float) -> void:
 	hitbox.global_position.y = Globals.player.global_position.y
-	closing_in.global_position.y = Globals.player.global_position.y
-	close.global_position.y = Globals.player.global_position.y
 	
 	if not Debug.invulnerable: 
 		global_position.x += speed * delta
 	
+	var new_category = get_distance_category()
+	if new_category != current:
+		current = new_category
+		Events.music_change_requested.emit(current)
 
 func _on_hitbox_entered(area: Area2D) -> void:
 	if area is Sled:
 		Globals.player.crash()
 	
 
+func get_distance_to_player()->float:
+	return Globals.player.global_position.x - global_position.x
 
-func _on_close_area_entered(_area: Area2D) -> void:
-	Events.music_change_requested.emit(Types.GameMusic.HARD)
-
-
-func _on_close_area_exited(_area: Area2D) -> void:
-	Events.music_change_requested.emit(Types.GameMusic.NORMAL)
-
-
-func _on_closing_in_area_entered(_area: Area2D) -> void:
-	Events.music_change_requested.emit(Types.GameMusic.NORMAL)
-
-
-func _on_closing_in_area_exited(_area: Area2D) -> void:
-	Events.music_change_requested.emit(Types.GameMusic.EASY)
+func get_distance_category():
+	var d = get_distance_to_player()
+	if d < critical_distance:
+		return Types.GameMusic.HARD
+	if d < far_distance:
+		return Types.GameMusic.NORMAL
+	return Types.GameMusic.EASY
+	
+func _on_log_timer_timeout() -> void:
+	Logger.info("Avalanche distance: %d (%s)" % \
+		[get_distance_to_player(), get_distance_category()])
