114b9f17cb4c6149e37f15451bb880cd493ec4b0
Cláudio Coelho <claudio.coelho@solenix.ch>
2024-12-22 15:32:36 +0000

    start screen adjustments

--- godot/src/start_screen/start_screen.gd
+++ godot/src/start_screen/start_screen.gd
@@ -9,5 +9,8 @@ func _ready() -> void:
 
 func _on_start_button_pressed() -> void:
 	$sfx_button.play()
+	$StartButton.disabled=true
+	if $sfx_button.stream:
+		await $sfx_button.finished
 	Globals.start_game()
 	
